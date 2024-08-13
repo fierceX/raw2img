e1c37d510b6c05b878967c13670ad29a5fa86df6
fiercex <fiercex@outlook.com>
2024-08-13 17:01:18 +0800

    fix

--- raw/src/raw.rs
+++ raw/src/raw.rs
@@ -1,12 +1,23 @@
 
-use std::{fs::{self, File}, io::BufWriter};
+use std::{default, fs::{self, File}, io::{BufReader, BufWriter, Write}, path::Path};
 use std::ffi::{c_char, c_int, CString};
+// use exif::Exif;
 use image::{DynamicImage, ExtendedColorType, ImageBuffer, ImageEncoder};
+use img_parts::{jpeg::Jpeg, Bytes, ImageEXIF};
 use rayon::prelude::*;
 use libraw_rs_vendor::{
     libraw_close, libraw_data_t, libraw_dcraw_make_mem_image, libraw_dcraw_process, libraw_init,
     libraw_open_file, libraw_processed_image_t, libraw_unpack, LibRaw_errors_LIBRAW_SUCCESS,
 };
+// use nom_exif::*;
+// use nom_exif::ExifTag::*;
+
+use exif::{Field, In, Tag, Value};
+use exif::experimental::Writer;
+
+// use img_parts::jpeg::Jpeg;
+// use img_parts::{ImageEXIF, ImageICC};
+use jpeg_encoder;
 use webp::Encoder;
 mod lut3d;
 use crate::lut3d::{interp_8_tetrahedral, parse_cube};
@@ -16,6 +27,18 @@ pub struct RawData {
     width: i32,
     height: i32,
     colors: i32,
+    iso:f32,
+    aperture:f32,
+    shutter:f32,
+    focal_len:u16,
+}
+
+#[derive(Debug)]
+pub struct Myexif {
+    iso:f32,
+    aperture:f32,
+    shutter:f32,
+    focal_len:u16,
 }
 
 fn exposure_shift(data: &[u8]) -> f32 {
@@ -58,7 +81,65 @@ fn exposure_shift(data: &[u8]) -> f32 {
     v
 }
 
-fn read_raw(input: String, wb: bool, half_size: bool, exp_shift: f32, threshold: i32) -> RawData {
+fn generate_gamma_lut(lut: &mut [u8], gamma: f32) {
+    println!("{:?}",gamma);
+    for i in 0..=TBLN {
+        lut[i] = ((i as f32/255.0).powf(1.0/gamma) * 255.0).round() as u8;
+    }
+}
+
+const TBLN: usize = 255;
+
+fn generate_linear_lut(lut:&mut [u8],shift: f32,smooth:f32){
+    let x1: f32;
+    let x2: f32 = TBLN as f32;
+    let y1: f32;
+    let y2: f32;
+
+    let cstops = shift.ln() / 2.0f32.ln();
+    let room = cstops * 2.0;
+    let roomlin = 2.0f32.powf(room);
+    x1 = (x2 + 1.0) / roomlin - 1.0;
+    y1 = x1 * shift;
+    y2 = x2 * (1.0 + (1.0 - smooth) * (shift - 1.0));
+    let sq3x = (x1 * x1 * x2).powf(1.0 / 3.0);
+    let b = (y2 - y1 + shift * (3.0 * x1 - 3.0 * sq3x)) / (x2 + 2.0 * x1 - 3.0 * sq3x);
+    let a = (shift - b) * 3.0 * (x1 * x1).powf(1.0 / 3.0);
+    let cc = y2 - a * x2.powf(1.0 / 3.0) - b * x2;
+
+    for i in 0..=TBLN {
+        let x = i as f32;
+        let y = a * x.powf(1.0 / 3.0) + b * x + cc;
+        if i < x1 as usize {
+            lut[i] = (x * shift) as u8;
+        } else {
+            lut[i] = if y < 0.0 { 0 } else if y > TBLN as f32 { TBLN as u8 } else { y as u8 };
+        }
+    }
+}
+
+fn apply_exp_correction(image: &mut Vec<u8>, shift: f32,luttype:&str){
+    let mut lut = [0u8; TBLN + 1];
+    let smooth = 0.9f32; // 示例值
+    match luttype {
+        "gamma" => {
+            generate_gamma_lut(&mut lut,shift);
+        }
+        "linear" => {
+            generate_linear_lut(&mut lut,shift,smooth);
+        }
+        _default =>{
+            generate_linear_lut(&mut lut,shift,smooth);
+        }
+    }
+
+    for channel in image.iter_mut(){
+        *channel = lut[*channel as usize];
+    }
+
+}
+
+fn read_raw(input: &str, wb: bool, half_size: bool, exp_shift: f32, threshold: i32) -> RawData {
     let libraw_data: *mut libraw_data_t = unsafe { libraw_init(0) };
     let rust_var = 0;
     let input_c_str = CString::new(input).unwrap();
@@ -68,6 +149,10 @@ fn read_raw(input: String, wb: bool, half_size: bool, exp_shift: f32, threshold:
         libraw_unpack(libraw_data);
         (*(libraw_data)).params.exp_correc = 1;
         (*(libraw_data)).params.exp_preser = 0.8;
+        let iso = (*(libraw_data)).other.iso_speed;
+        let aperture = (*(libraw_data)).other.aperture;
+        let shutter = (*(libraw_data)).other.shutter;
+        let focal_len = (*(libraw_data)).lens.FocalLengthIn35mmFormat;
         if wb {
             (*(libraw_data)).params.use_auto_wb = 1;
         } else {
@@ -115,6 +200,10 @@ fn read_raw(input: String, wb: bool, half_size: bool, exp_shift: f32, threshold:
             width: _width,
             height: _height,
             colors: _colors,
+            iso,
+            aperture,
+            shutter,
+            focal_len,
         };
         libraw_close(libraw_data);
         rawdata
@@ -122,7 +211,7 @@ fn read_raw(input: String, wb: bool, half_size: bool, exp_shift: f32, threshold:
 }
 
 
-fn save(output:String,data:Vec<u8>,width:u32,height:u32,quality:i32) -> Result<String,String> {
+fn save(output:String,data:Vec<u8>,width:u32,height:u32,quality:i32,_exif:&Myexif) -> Result<String,String> {
     match output.split('.').last() {
         Some(suffix) if suffix == "webp" =>{
                 let encoder = Encoder::from_rgb(&data, width, height);
@@ -131,16 +220,62 @@ fn save(output:String,data:Vec<u8>,width:u32,height:u32,quality:i32) -> Result<S
                 Ok("aaa".to_string())
         }
         Some(suffix) if suffix == "jpg" => {
-            let file = File::create(output).unwrap();
-            let ref mut buf_writer = BufWriter::new(file);
-            let encoder = image::codecs::jpeg::JpegEncoder::new_with_quality(buf_writer, quality.try_into().unwrap());
-                encoder.write_image(
-                    &data,
-                    width,
-                    height,
-                    ExtendedColorType::Rgb8,
-                ).unwrap();
-                Ok("aaa".to_string())
+            {
+                let mut buf_writer = BufWriter::new(std::io::Cursor::new(Vec::new()));
+            let mut encoder = jpeg_encoder::Encoder::new(& mut buf_writer, quality.try_into().unwrap());
+            encoder.set_progressive(true);
+            encoder.encode(&data, width.try_into().unwrap(), height.try_into().unwrap(), jpeg_encoder::ColorType::Rgb).unwrap();
+
+            let image_desc1 = Field {
+                tag: Tag::ExposureTime,
+                ifd_num: In::PRIMARY,
+                value: Value::Float(vec![_exif.shutter]),
+            };
+
+            let image_desc2 = Field {
+                tag: Tag::FNumber,
+                ifd_num: In::PRIMARY,
+                value: Value::Float(vec![_exif.aperture]),
+            };
+
+            let image_desc3 = Field {
+                tag: Tag::FocalLength,
+                ifd_num: In::PRIMARY,
+                value: Value::Short(vec![_exif.focal_len]),
+            };
+
+            let image_desc4 = Field {
+                tag: Tag::PhotographicSensitivity,
+                ifd_num: In::PRIMARY,
+                value: Value::Long(vec![_exif.iso as u32]),
+            };
+            
+
+            let mut writer = Writer::new();
+            let mut buf = std::io::Cursor::new(Vec::new());
+            writer.push_field(&image_desc1);
+            writer.push_field(&image_desc2);
+            writer.push_field(&image_desc3);
+            writer.push_field(&image_desc4);
+            // writer.push_field(&image_desc5);
+            writer.write(&mut buf, false).unwrap();
+
+            let output2 = File::create(&output).unwrap();
+
+            let cursor = buf_writer.into_inner().unwrap();
+            let buffer = cursor.into_inner();
+
+            let mut jpeg = Jpeg::from_bytes(buffer.into()).unwrap();
+            jpeg.set_exif(Some(Bytes::copy_from_slice(&buf.into_inner())));
+            jpeg.encoder().write_to(output2).unwrap();
+        }
+                
+
+            // let f: Value = exif.get_field(Tag, In::PRIMARY).unwrap().value;
+
+            // println!("f:{} s:{} l:{} i:{}",aperture,shutter,focal_len,iso);
+            
+            Ok("aaa".to_string())
         }
         Some(_) => {
             let img2: image::DynamicImage =
@@ -158,18 +293,19 @@ fn save(output:String,data:Vec<u8>,width:u32,height:u32,quality:i32) -> Result<S
 
 pub fn raw_process(input:String,output:String,lut:String,wb:bool,half_size:bool,exp_shift:f32,threshold:i32,quality:i32) -> Result<String,String> {
     if let Ok(_) = fs::metadata(&input){
-        let rawdata = read_raw(input, wb, half_size, exp_shift, threshold);
+        let rawdata = read_raw(&input, wb, half_size, exp_shift, threshold);
+        let _exif = Myexif{iso:rawdata.iso,aperture:rawdata.aperture,shutter:rawdata.shutter,focal_len:rawdata.focal_len};
         if let Ok(_) = fs::metadata(&lut){
             let lut3d = parse_cube(&lut).unwrap();
             let img = interp_8_tetrahedral(lut3d,rawdata.data,rawdata.width,rawdata.colors);
-            save(output, img, rawdata.width.try_into().unwrap(), rawdata.height.try_into().unwrap(), quality)
+            save(output, img, rawdata.width.try_into().unwrap(), rawdata.height.try_into().unwrap(), quality,&_exif)
         }
         else{
-            save(output, rawdata.data, rawdata.width.try_into().unwrap(), rawdata.height.try_into().unwrap(), quality)
+            save(output, rawdata.data, rawdata.width.try_into().unwrap(), rawdata.height.try_into().unwrap(), quality,&_exif)
         }
     }
     else {
-        Ok("aaa".to_string())
+        Err("aaa".to_string())
     }
     
 }
